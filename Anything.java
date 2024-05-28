381f6117f6d20a178dfd12063bf680447472ed3b
Sit Lo <141769635+Official3Lo@users.noreply.github.com>
2024-05-27 20:49:28 -0700

    Update Anything.java

--- Anything.java
+++ Anything.java
@@ -1,40 +1,41 @@
-// use the Scanner class
 import java.util.Scanner;
-public class Anything {
-    public static void main (String[] args){ //remember this
-        System.out.println("\n\n Welcome to the largest numbers program\n\n"); // print to your monitor
-        int num1 = 0;
-        int num2 = 0;
-        int num3 = 0;
+import java.io.*;
+import java.util.*;
 
-        Scanner myScanner = new Scanner(System.in);
+public class Main {
 
-        // Prompt the user for the first number
-        System.out.println("\n\nPlease enter the first number\n\n");
-        int num1 = myScanner.nextInt();
+    public static void main(String[] args) {
+        System.out.println("+-------------------+\n"
+                            +"|    SIT Program    |\n"
+                            +"+-------------------+\n\n");
 
-        System.out.println("you entered: " + num1);
+        //Data variable
+        int num1=0,num2=0,num3=0, largest = 0;
+        Scanner sc = new Scanner(System.in);
 
+        //Input value for data
+        System.out.println("Enter a num: ");
+        num1 = sc.nextInt();
 
-        System.out.println("\n\nPlease enter the second number\n\n");
-        int num2 = myScanner.nextInt();
+        System.out.println("Enter a num: ");
+        num2 = sc.nextInt();
 
-        System.out.println("you entered: " + num2);
+        System.out.println("Enter a num: ");
+        num3 = sc.nextInt();
 
-        System.out.println("\n\nPlease enter the third number\n\n");
-        int num3 = myScanner.nextInt();
+        System.out.println("All numbers entered are ["+num1+", "+num2+", "+num3+"]");
 
-        System.out.println("you entered: " + num3);
-
-
-        System.out.println("\n\nAll the numbers you had entered are: \n\n" + num1 + "and" + num2 + "and" + num3);
-        int largest = num1;
-        if (num2 > largest){
-            largest = num2;
+        //Data analyzation
+        if(num2>largest){
+            largest=num2;
         }
         if (num3>largest){
-            largest = num3;
+            largest=num3;
         }
-        System.out.println("\n\nThe largest number is: \n\n" + largest);
+
+        //Final output of largest num
+        System.out.println("Largest number is: "+ largest);
+
     }
 }
+
